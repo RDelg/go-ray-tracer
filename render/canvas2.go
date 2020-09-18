f4cb948f7ad1725887f2bb68c3b993e88e094fd1
Rene Delgado <rjdelgadoc@falabella.cl>
2020-09-18 00:16:35 -0300

    Add CreateCanvas2 and WritePixel methods

--- render/canvas2.go
+++ render/canvas2.go
@@ -2,9 +2,30 @@ package render
 
 import "github.com/RDelg/go-ray-tracer/math32"
 
-// Canvas2 is a 2D canvas with width and height WxH
+// Canvas2 is a 2D canvas with width and height whH
 type Canvas2 struct {
-	W      int
-	H      int
+	width  int
+	height int
 	screen [][]math32.Color
 }
+
+// CreateCanvas2 creates a Canvas2 with the given width and height
+func CreateCanvas2(width, height int) *Canvas2 {
+	matrix := make([][]math32.Color, width)
+	for i := 0; i < width; i++ {
+		matrix[i] = make([]math32.Color, height)
+	}
+	return &Canvas2{width: width, height: height, screen: matrix}
+}
+
+// WritePixel writes the color in the position x,y in the canvas
+// Returns a pointer to the updated canvas
+func (c *Canvas2) WritePixel(x, y int, color *math32.Color) *Canvas2 {
+	c.screen[x][y] = *color
+	return c
+}
+
+// ToPPM returns the canvas values as PPM format
+// func (c *Canvas2) ToPPM() string {
+// 	return
+// }
