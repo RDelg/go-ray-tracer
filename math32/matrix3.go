2c456ed466e19d54c6d361a2ace6f430464e7b44
Rene Delgado <rjdelgadoc@falabella.cl>
2020-09-18 00:01:31 -0300

    Add new methods to Matrix3

--- math32/matrix3.go
+++ math32/matrix3.go
@@ -20,3 +20,61 @@ func Matrix3Zeros() *Matrix3 {
 func Matrix3Identity() *Matrix3 {
 	return &Matrix3{}
 }
+
+// Add adds the other Matrix3 to this one
+// Returns a pointer to the updated Matrix3
+func (m *Matrix3) Add(other *Matrix3) *Matrix3 {
+	m.Values[0][0] += other.Values[0][0]
+	m.Values[0][1] += other.Values[0][1]
+	m.Values[0][2] += other.Values[0][2]
+
+	m.Values[1][0] += other.Values[1][0]
+	m.Values[1][1] += other.Values[1][1]
+	m.Values[1][2] += other.Values[1][2]
+
+	m.Values[2][0] += other.Values[0][0]
+	m.Values[2][1] += other.Values[0][1]
+	m.Values[2][2] += other.Values[0][2]
+	return m
+}
+
+// Mult multiplies the other Matrix3 to this one
+// Returns a pointer to the updated Matrix3
+func (m *Matrix3) Mult(other *Matrix3) *Matrix3 {
+
+	t := Matrix3{}
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			t.Values[i][j] += m.Values[i][j] * other.Values[j][i]
+		}
+	}
+	m = &t
+	return m
+}
+
+// Transpose transpose the matrix
+// Returns a pointer to the updated Matrix3
+func (m *Matrix3) Transpose() *Matrix3 {
+	m.Values[1][0], m.Values[0][1] = m.Values[0][1], m.Values[1][0]
+	m.Values[2][0], m.Values[0][2] = m.Values[0][2], m.Values[2][0]
+	m.Values[2][1], m.Values[1][2] = m.Values[1][2], m.Values[2][1]
+	return m
+}
+
+// TODO
+// func (m *Matrix3) SubMatrix2(n, m, int) math32.Matrix2 {
+// }
+// func (m *Matrix3) Determinant() float32 {
+// }
+// func (m *Matrix3) Equal(other Matrix3) bool {
+// }
+// func (m *Matrix3) Determinant() float32 {
+// }
+//  func (m *Matrix3) AddVector3(vector math.Vector3) *Matrix3 {
+// }
+//  func (m *Matrix3) MultVector3(vector math.Vector3) *Matrix3 {
+// }
+//  func (m *Matrix3) AddScalar3(scalar float32) *Matrix3 {
+// }
+//  func (m *Matrix3) MultScalar3(scalar float32) *Matrix3 {
+// }
