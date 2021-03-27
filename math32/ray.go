fcb464498dcd91001760e2ead506c7fe77b43e32
Rene Delgado <rene854@gmail.com>
2021-03-27 14:36:27 -0300

    Add ray tests

--- math32/ray.go
+++ math32/ray.go
@@ -11,8 +11,8 @@ func NewRay(origin *Point3, direction *Vector3) *Ray {
 	return &Ray{origin, direction}
 }
 
-func (r *Ray) Position(time float32) *Vector3 {
-	return &Vector3{
+func (r *Ray) Position(time float32) *Point3 {
+	return &Point3{
 		r.Origin.X + r.Direction.X*time,
 		r.Origin.Y + r.Direction.Y*time,
 		r.Origin.Z + r.Direction.Z*time,
--- math32/ray_test.go
+++ math32/ray_test.go
@@ -97,3 +97,71 @@ func TestRay_Intersect(t *testing.T) {
 		})
 	}
 }
+
+func TestRay_Position(t *testing.T) {
+	type fields struct {
+		Origin    *Point3
+		Direction *Vector3
+	}
+	type args struct {
+		time float32
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Point3
+	}{
+		{
+			"Runs corretly",
+			fields{
+				&Point3{1., 2., 3.},
+				&Vector3{4., 5., 6.},
+			},
+			args{2.0},
+			&Point3{9., 12., 15.},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Ray{
+				Origin:    tt.fields.Origin,
+				Direction: tt.fields.Direction,
+			}
+			if got := r.Position(tt.args.time); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ray.Position() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewRay(t *testing.T) {
+	type args struct {
+		origin    *Point3
+		direction *Vector3
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Ray
+	}{
+		{
+			"Runs corretly",
+			args{
+				&Point3{1., 2., 3.},
+				&Vector3{4., 5., 6.},
+			},
+			&Ray{
+				&Point3{1., 2., 3.},
+				&Vector3{4., 5., 6.},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewRay(tt.args.origin, tt.args.direction); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
